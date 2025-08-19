68549718dca13c19416a36647701ac41464adcaf
Adam Nowacki <ad.nowacki@gmail.com>
2025-08-19 21:18:30 +0200

    Add Apollo

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp, h, provide } from 'vue';
+import { DefaultApolloClient } from '@vue/apollo-composable';
+import { apolloClient } from './apollo';
 
-createApp(App).mount('#app')
+import './style.css';
+import App from './App.vue';
+
+createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+});
