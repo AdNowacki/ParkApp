90fc5518bdfe05c4cab4cf8aa5c795d0b699ec4a
Adam Nowacki <ad.nowacki@gmail.com>
2025-08-23 01:19:00 +0200

    Update Home Page

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./app.vue', './src/**/*.{html,js,ts,jsx,tsx}', './formkit.theme.ts'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
