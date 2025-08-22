ac2d947d52d03431bfab34529249e7e006e64bc8
Adam <ad.nowacki@gmail.com>
2025-08-23 01:22:20 +0200

    Change project structure (#18)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '@/pages/Home/Home.vue';
-import Login from '@/pages/Login/Login.vue';
-import NotFound from '@/common/NotFound/NotFound.vue';
+import Home from '@/pages/Home.vue';
+import Login from '@/pages/Login.vue';
+import NotFound from '@/common/NotFound.vue';
 import appConfig from '~~/app.config.js';
 
 const routes = [
