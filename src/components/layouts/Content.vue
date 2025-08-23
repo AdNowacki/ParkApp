92238a3aca192ff51af76911646891718b339396
Adam Nowacki <ad.nowacki@gmail.com>
2025-08-23 23:07:30 +0200

    Create Content WIP

---
+++ src/components/layouts/Content.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import PageContainer from '@/layouts/PageContainer.vue';
+</script>
+<template>
+  <PageContainer class="px-0">
+    <div>
+      <slot></slot>
+    </div>
+  </PageContainer>
+</template>
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
   import PageHeader from '@/layouts/PageHeader.vue';
+  import Content from '@/layouts/Content.vue';
   import Navigation from '@/layouts/Navigation.vue';
 </script>
 
 <template>
   <PageHeader />
+  <Content class="mt-10">
+    <div class="flex">
+      <div>Pilot</div>
+      <div>Lista</div>
+    </div>
+    <div>Paginacja</div>
+  </Content>
   <Navigation />
 </template>
