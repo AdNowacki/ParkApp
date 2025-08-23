f0430aa72f7cf19bb2fca0e49a0b7c60132f2181
Adam Nowacki <ad.nowacki@gmail.com>
2025-08-23 23:24:31 +0200

    Create Content structure

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -2,16 +2,17 @@
   import PageHeader from '@/layouts/PageHeader.vue';
   import Content from '@/layouts/Content.vue';
   import Navigation from '@/layouts/Navigation.vue';
+  import Pagination from '@/ui/Pagination.vue';
 </script>
 
 <template>
   <PageHeader />
   <Content class="mt-10">
-    <div class="flex">
-      <div>Pilot</div>
+    <div class="flex gap-10">
+      <div class="grow-0 shrink-0 basis-[33%]">Pilot</div>
       <div>Lista</div>
     </div>
-    <div>Paginacja</div>
+    <Pagination :total="10" :current="1" />
   </Content>
   <Navigation />
 </template>
---
+++ src/components/ui/Pagination.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import { computed, withDefaults, defineProps } from 'vue';
+
+  const props = withDefaults(
+    defineProps<{
+      total?: number;
+      perPage?: number;
+      current?: number;
+    }>(),
+    {
+      total: 0,
+      perPage: 4,
+      current: 1,
+    }
+  );
+
+  const totalPages = computed(() => {
+    return Math.ceil(props.total / props.perPage);
+  });
+</script>
+<template>
+  <div v-if="totalPages > 1" class="max-w-3xl w-full mx-auto px-4 lg:p-0">totalPages: {{ totalPages }}</div>
+</template>
