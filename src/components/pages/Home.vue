b838edc82bfc53107f63a102365b201e5cfbf4fd
Adam Nowacki <ad.nowacki@gmail.com>
2025-08-23 08:16:46 +0200

    Create Navigation component

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
   import PageHeader from '@/layouts/PageHeader.vue';
+  import Navigation from '@/layouts/Navigation.vue';
 </script>
 
 <template>
   <PageHeader />
+  <Navigation />
 </template>
