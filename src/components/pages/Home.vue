adf5db116b04ea933b1f45520e9bab7660db8f08
Adam <ad.nowacki@gmail.com>
2025-08-24 09:19:28 +0200

    Create RemoteControl component (#28)
    * Create Remote component
    
    * Add nvmrc
    
    * Add translations
    
    * Rename component

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,22 +1,26 @@
 <script setup lang="ts">
   import { ref } from 'vue';
+  import { useI18n } from 'vue-i18n';
   import PageHeader from '@/layouts/PageHeader.vue';
   import Content from '@/layouts/Content.vue';
   import Navigation from '@/layouts/Navigation.vue';
-  import DotPagination from '~/components/ui/DotPagination.vue';
+  import DotPagination from '@/ui/DotPagination.vue';
+  import RemoteControl from '@/ui/RemoteControl.vue';
   import Gates from '@/common/Gates.vue';
+
+  const { t } = useI18n();
 
   const currentPage = ref<number>(1);
 
   const remoteMockData = {
-    title: 'Długa nazwa pilota',
+    title: t('mock.title'),
     items: [
-      { id: 1, label: 'Szlaban Grzybowska 1' },
-      { id: 2, label: 'Brama garażowa #1' },
-      { id: 3, label: 'Brama garażowa #2' },
-      { id: 4, label: 'Szlaban Powstańsów Śląskich 32' },
+      { id: 1, label: 'Szlaban Grzybowska 1', remoteLabel: 'A' },
+      { id: 2, label: 'Brama garażowa #1', remoteLabel: 'B' },
+      { id: 3, label: 'Brama garażowa #2', remoteLabel: 'C' },
+      { id: 4, label: 'Szlaban Powstańsów Śląskich 32', remoteLabel: 'D' },
     ],
-    hint: 'Wybierz bramę, by otworzyć',
+    hint: t('mock.hint'),
   };
 
   const activeGate = ref<number | undefined>(remoteMockData.items[0].id);
@@ -27,8 +31,10 @@
   <Content class="mt-10">
     <div class="pb-15">
       <div class="flex">
-        <div class="grow-0 shrink-0 md:w-[50%] w-[33%]">Pilot</div>
-        <div class="grow-0 shrink-0 md:w-[50%] w-[67%]">
+        <div class="grow-0 shrink-0 md:w-[50%] w-[33%] py-6">
+          <RemoteControl :items="remoteMockData.items" />
+        </div>
+        <div class="grow-0 shrink-0 md:w-[50%] w-[67%] p-6">
           <Gates
             v-model="activeGate"
             :title="remoteMockData.title"
